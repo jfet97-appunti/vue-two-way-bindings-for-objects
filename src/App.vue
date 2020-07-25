f769af748e8981c8ff12deb5b4f4d0faa703eef0
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-07-25 18:32:44 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Padre/>
+</template>
+
+<script>
+import Padre from "./components/Padre";
+
+export default {
+  name: "App",
+  components: {
+    Padre
+  }
+};
+</script>
+
---
+++ src/components/Figlio.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Figlio</h1>
+    <pre>
+      <code>
+        {{ formattedObj }}
+      </code>
+    </pre>
+    <button @click="randomUpdateObj">Aggiorna k2 dal figlio</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Figlio",
+  props: {
+    obj: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    formattedObj() {
+      return JSON.stringify(this.obj, null, 4);
+    }
+  },
+  methods: {
+    randomUpdateObj() {
+      const newK2 = Math.floor(Math.random() * (100 - 1) + 1);
+      this.$emit("update:obj", { ...this.obj, k2: newK2 });
+    }
+  }
+};
+</script>
+
