dd80a55e40ccd3359707ce3493399f1dfcee330c
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-07-25 18:34:42 +0200

    initial commit

---
+++ src/components/Figlio.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Figlio</h1>
+    <pre>
+      <code>
+        {{ formattedObj }}
+      </code>
+    </pre>
+    <input type="text" v-model="localK1">
+    <button @click="randomUpdateK2">Aggiorna k2 dal figlio</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Figlio",
+  props: {
+    k1: {
+      type: String,
+      required: true
+    },
+    k2: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    formattedObj() {
+      return JSON.stringify({ k1: this.localK1, k2: this.k2 }, null, 4);
+    },
+    localK1: {
+      get() {
+        return this.k1;
+      },
+      set(newK1) {
+        this.$emit("update:k1", newK1);
+      }
+    }
+  },
+  methods: {
+    randomUpdateK2() {
+      const newK2 = Math.floor(Math.random() * (100 - 1) + 1);
+      this.$emit("update:k2", newK2);
+    }
+  }
+};
+</script>
+
