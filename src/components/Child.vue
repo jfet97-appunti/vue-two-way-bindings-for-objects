122d8f5b6bc7ebb58e8b2c8a461faded08f21e76
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-07-26 19:26:08 +0200

    add english

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <h1>Figlio</h1>
+    <h1>Child</h1>
     <pre>
       <code>
         {{ formattedObj }}
       </code>
     </pre>
-    <button @click="randomUpdateObj">Aggiorna k2 dal figlio</button>
+    <button @click="randomUpdateObj">Update k2 from child</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Figlio",
+  name: "Child",
   props: {
     obj: {
       type: Object,
@@ -35,6 +35,8 @@ export default {
   methods: {
     randomUpdateObj() {
       const newK2 = Math.floor(Math.random() * (100 - 1) + 1);
+      // the reference needs to be changed to trigger the reference
+      // changing a prop is not enough
       this.localObj = { ...this.localObj, k2: newK2 };
     }
   }
